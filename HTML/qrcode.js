77536f25bc1ea25b975879a94b0b4cca7164778b
joyi911 <139338691+joyi911@users.noreply.github.com>
2023-08-30 16:36:23 +0800

    Add files via upload

---
+++ HTML/qrcode.js
@@ -0,0 +1,26 @@
+//imgname 图片名称，linecode：line 号码，二维码图片放到qrcode文件夹里面
+var qrcodes=["LBJRs9x0FuY1CRCzXRDcLn","KsgMZCKiHkgCi4kawkDi2J","DLc900V0FA7L60M3V54L2M"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
